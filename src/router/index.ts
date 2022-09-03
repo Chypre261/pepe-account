2523b1db20849ad735104ea6fca1e0e2711c91eb
chypre <liang_liuyi@qq.com>
2022-09-03 11:22:01 +0800

    add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, {RouteConfig} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -11,14 +12,6 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     redirect: '/money'
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/money',
@@ -34,6 +27,11 @@ const routes: Array<RouteConfig> = [
     path: '/statistics',
     name: 'Statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    name: '404',
+    component: NotFound
   }
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      Address not found
+    </div>
+    <div>
+      <router-link to="/">Return to Home page</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound.vue'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
