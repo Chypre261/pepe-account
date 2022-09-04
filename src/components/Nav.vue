80fc30ae4644c7826a902157f0a96b4d25befef9
chypre <liang_liuyi@qq.com>
2022-09-04 20:29:47 +0800

    try importing svg icons

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="nav">
-    <router-link to="/money">Input</router-link>
+
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#labels"></use>
+      </svg>
+      Labels
+    </router-link>
     |
-    <router-link to="/labels">Labels</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"></use>
+      </svg>
+      Input
+    </router-link>
     |
-    <router-link to="/statistics">Statistics</router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"></use>
+      </svg>
+      Statistics
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Nav'
-};
+  import x from '@/assets/icons/labels.svg';
+  import y from '@/assets/icons/money.svg';
+  import z from '@/assets/icons/statistics.svg';
+
+  console.log(x);
+  console.log(y);
+  console.log(z);
+
+  export default {
+    name: 'Nav'
+  };
 </script>
 
 <style lang="scss" scoped>
