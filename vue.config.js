47df0a6f4d31e245e0dd2383f88597c3ca8c7338
chypre <liang_liuyi@qq.com>
2022-09-04 23:00:44 +0800

    use svgo-loader to ignore svg fill attribute

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,8 @@ module.exports = {
       .include.add(dir).end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
       .options({extract: false}).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir)
