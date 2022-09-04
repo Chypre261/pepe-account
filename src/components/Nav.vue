6027f423645e210a291a52d6a58be0f359f74b22
chypre <liang_liuyi@qq.com>
2022-09-04 21:46:00 +0800

    create component Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,37 +2,23 @@
   <div class="nav">
 
     <router-link to="/labels">
-      <svg>
-        <use xlink:href="#labels"></use>
-      </svg>
+      <Icon name="labels" />
       Labels
     </router-link>
-    |
+
     <router-link to="/money">
-      <svg>
-        <use xlink:href="#money"></use>
-      </svg>
+      <Icon name="money" />
       Input
     </router-link>
-    |
+
     <router-link to="/statistics">
-      <svg>
-        <use xlink:href="#statistics"></use>
-      </svg>
+      <Icon name="statistics" />
       Statistics
     </router-link>
   </div>
 </template>
 
 <script lang="ts">
-  import x from '@/assets/icons/labels.svg';
-  import y from '@/assets/icons/money.svg';
-  import z from '@/assets/icons/statistics.svg';
-
-  console.log(x);
-  console.log(y);
-  console.log(z);
-
   export default {
     name: 'Nav'
   };
