ada487a1b9449ee24c4d0b3eb3acbb5c0f36ce36
chypre <liang_liuyi@qq.com>
2022-09-04 22:49:25 +0800

    active-class

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,19 @@
 <template>
   <nav>
 
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels" />
+      Label
     </router-link>
 
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money" />
+      Add
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics" />
+      Statistics
     </router-link>
   </nav>
 </template>
@@ -26,12 +29,17 @@
     display: flex;
     flex-direction: row;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.7);
+    font-size: 12px;
     > .item {
+      padding: 3px;
       width: 33.33333%;
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
+      &.selected {
+        color: green;
+      }
       .icon {
         width: 3em;
         height: 3em;
