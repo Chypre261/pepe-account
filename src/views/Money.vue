c93ef7922fcf250d8e6b4ce5c6ee950533e847ce
chypre <liang_liuyi@qq.com>
2022-09-05 15:51:59 +0800

    add global style and global scss variable

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,41 @@
 <template>
   <Layout>
-    Money.vue
+
+    <ul class="class">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+    <div class="new">
+      新建标签
+    </div>
+    <div class="note">
+      <label class="name">备注
+        <input type="text">
+      </label>
+    </div>
+    <div>
+      <ul>
+        <li>支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>删除</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>清空</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>OK</button>
+      <button>0</button>
+    </div>
   </Layout>
 </template>
 
