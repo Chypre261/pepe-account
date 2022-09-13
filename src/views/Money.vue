a4810fbaed9fa84b4bd0472243894dcad5f92c46
chypre <liang_liuyi@qq.com>
2022-09-13 22:34:47 +0800

    save deep copy of record into the list in localStorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
+    {{ record }}
     <NumberPad :value.sync="record.amount"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
@@ -14,7 +14,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
 
 type Record = {
   tags: string[],
@@ -29,6 +29,7 @@ type Record = {
 
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
+  recordList: Record [] = [];
   record: Record = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -38,7 +39,17 @@ export default class Money extends Vue {
   }
 
   onUpdateNotes(value: string) {
-    this.record.notes   = value;
+    this.record.notes = value;
+  }
+
+  saveRecord(value: string[]) {
+    const recordDeep = JSON.parse(JSON.stringify(this.record));
+    this.recordList.push(recordDeep);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem ('recordList', JSON.stringify(this.recordList));
   }
 
 };
