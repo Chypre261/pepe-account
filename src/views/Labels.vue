45c9d0a1a9349d91ea889755ccb113039bfb47fd
chypre <liang_liuyi@qq.com>
2022-09-22 00:38:04 +0800

    revise Button to a common component refactor Notes to FormItem

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,13 +1,15 @@
 <template>
   <Layout>
     <div class="tags">
-      <RouterLink class="tag" v-for="tag in tags" :key="tag"
+      <RouterLink class="tag" v-for="tag in tags" :key="tag.id"
                   :to="`/labels/edit/${tag.id}`">
         <span>{{ tag.name }}</span>
       </RouterLink>
     </div>
     <div class="createTag-wrapper">
-      <button class="createTag" @click="createTag">新建标签</button>
+      <Button class="createTag"
+              @click="createTag">新建标签
+      </Button>
     </div>
   </Layout>
 </template>
