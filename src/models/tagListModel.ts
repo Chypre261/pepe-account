2d4f787eba5fb396066f707b03e5ecb98c4801a2
chypre <liang_liuyi@qq.com>
2022-09-25 23:35:08 +0800

    edit tag

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -7,16 +7,36 @@ type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated'; // 'success' or 'duplicated'
+  update: (name: string, id: string) => 'success' | 'not found' | 'duplicated'
   save: () => void;
 }
 const tagListModel: TagListModel = {
   data: [],
   create(name: string) {
-    const names = this.data.map(item=>item.name);
-    if (names.indexOf(name) >= 0) {return 'duplicated';}
+    const names = this.data.map(item => item.name);
+    if (names.indexOf(name) >= 0) {
+      return 'duplicated';
+    }
     this.data.push({name: name, id: name});
     this.save();
     return 'success';
+  },
+  update(id: string, name: string) {
+    const idList = this.data.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.data.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return 'success';
+      }
+
+    } else {
+      return 'not found';
+    }
   },
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,9 +1,16 @@
 <template>
   <Layout>
-    <div>
-      <span>
-        编辑标签
-      </span>
+    <div class="navBar">
+      <span class="title">编辑标签</span>
+    </div>
+    <div class="form-wrapper">
+      <FormItem :value="tag.name"
+                @update:value="updateTag"
+                field-name="标签名" placeholder="请输入标签名">
+      </FormItem>
+    </div>
+    <div class="button-wrapper">
+      <Button>删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -12,18 +19,30 @@
 import tagListModel from '@/models/tagListModel';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import FormItem from '@/components/Money/FormItem.vue';
+import Button from '@/components/Button.vue';
 
-@Component
+@Component({
+  components: {Button, FormItem}
+})
 export default class EditLabel extends Vue {
+  tag?: { id: string, name: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch;
     const tags = tagListModel.data;
     const tag = tags.filter(t => t.id === id)[0];
     if (tag) {
-      console.log(tag);
-    }else {
-      this.$router.replace('/404' );
+      this.tag = tag;
+    } else {
+      this.$router.replace('/404');
+    }
+  }
+
+  updateTag(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
     }
   }
 
@@ -31,32 +50,37 @@ export default class EditLabel extends Vue {
 </script>
 
 <style lang="scss" scoped>
-  .navBar {
-    text-align: center;
-    font-size: 16px;
-    padding: 12px 16px;
-    background: white;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    > .title {
-    }
-    > .leftIcon {
-      width: 24px;
-      height: 24px;
-    }
-    > .rightIcon {
-      width: 24px;
-      height: 24px;
-    }
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > .title {
   }
-  .form-wrapper{
-    background: white;
-    margin-top: 8px;
+
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
   }
-  .button-wrapper{
-    text-align: center;
-    padding: 16px;
-    margin-top: 44-16px;
+
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
   }
+}
+
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+}
+
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 </style>
