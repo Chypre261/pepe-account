341535a841f9a1aec091f51574f26484d2eff57d
chypre <liang_liuyi@qq.com>
2023-01-19 23:39:01 +0800

    Use Vuex in Money.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,7 +19,7 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index2.ts';
+import store from '@/store/index.ts';
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad}
@@ -29,6 +29,10 @@ export default class Money extends Vue {
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
+  created() {
+    this.$store.commit('fetchRecords');
+  }
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
